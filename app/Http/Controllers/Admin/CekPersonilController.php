ea9e048aa5badbbed20cb8283d837e58d5bbea1e
Wildan Belfiore <mikewildan@outlook.com>
2024-01-09 01:23:39 +0700

    pembuatan form dengan dynamic multi input pada Cek Personil telah rampung

--- app/Http/Controllers/Admin/CekPersonilController.php
+++ app/Http/Controllers/Admin/CekPersonilController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\CekPersonil;
 use App\Models\Pokja;
 use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
 class CekPersonilController extends Controller
@@ -27,21 +28,37 @@ class CekPersonilController extends Controller
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'nama_personil' => 'required|string|max:255',
-            'jabatan_personil' => 'required|string|max:255',
-            //'golongan_personil' => 'required|string|max:255',
-            'nik_personil' => 'required|string|max:255',
-            'npwp_personil' => 'required|string|max:255',
-            'email_personil' => 'required|email|max:255',
-            'telepon_personil' => 'required|string|max:255',
-        ]);
+        $requestData = $request->all();
 
-        CekPersonil::create($validatedData);
+        // Validasi setiap entri formulir
+        foreach ($requestData['nama_personil'] as $key => $namaPersonil) {
+            $validatedData = Validator::make([
+                'nama_personil' => $namaPersonil,
+                'jabatan_personil' => $requestData['jabatan_personil'][$key],
+                'nik_personil' => $requestData['nik_personil'][$key],
+                'npwp_personil' => $requestData['npwp_personil'][$key],
+                'email_personil' => $requestData['email_personil'][$key],
+                'telepon_personil' => $requestData['telepon_personil'][$key],
+            ], [
+                'nama_personil' => 'required|string|max:255',
+                'jabatan_personil' => 'required|string|max:255',
+                'nik_personil' => 'required|string|max:255',
+                'npwp_personil' => 'required|string|max:255',
+                'email_personil' => 'required|email|max:255',
+                'telepon_personil' => 'required|string|max:255',
+            ])->validate();
+
+            // Simpan setiap entri formulir
+            CekPersonil::create($validatedData);
+        }
 
         Alert::success('Success', 'Data cek personil berhasil disimpan.');
         return redirect()->route('admin.cek_personils.index');
     }
+
+
+
+
 
     public function show($id)
     {
