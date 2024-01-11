160779d2e735a9c092d1e90571a1beb2d25e6f0a
Wildan Belfiore <mikewildan@outlook.com>
2024-01-11 21:18:25 +0700

    UI pada fitur agenda  telah diperbarui

--- resources/views/admin/agenda/create.blade.php
+++ resources/views/admin/agenda/create.blade.php
@@ -1,48 +1,48 @@
 <x-app-layout>
-    <div class="container mx-auto mt-5">
+    <div class="container mx-auto mt-4 p-4">
         <!-- Page Heading -->
         <h1 class="text-3xl font-bold mb-2 text-gray-800">Tambah Data Agenda Rapat</h1>
-        <p class="mb-4 text-gray-600">Isi formulir di bawah ini untuk menambahkan data agenda rapat.</p>
+        <p class="text-gray-600 mb-4">Isi formulir di bawah ini untuk menambahkan data agenda rapat.</p>
 
         <!-- Form untuk Tambah Data -->
-        <div class="bg-white p-6 shadow-md rounded-md">
+        <div class="bg-white shadow-md rounded-md p-4">
             <form method="POST" action="{{ route('admin.agenda.store') }}">
                 @csrf
 
                 <!-- Email Peserta -->
-                <div class="mb-4">
-                    <label for="email_peserta" class="block text-sm font-medium text-gray-600">Email Peserta:</label>
-                    <input type="email" class="mt-1 p-2 w-full border rounded-md" id="email_peserta" name="email_peserta" required placeholder="Masukkan Email Peserta">
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                    <label for="email_peserta" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Peserta:</label>
+                    <input type="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="email_peserta" name="email_peserta" required placeholder="Masukkan Email Peserta">
                 </div>
 
                 <!-- Tanggal -->
-                <div class="mb-4">
-                    <label for="tanggal" class="block text-sm font-medium text-gray-600">Tanggal:</label>
-                    <input type="date" class="mt-1 p-2 w-full border rounded-md" id="tanggal" name="tanggal" required placeholder="Pilih Tanggal">
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                    <label for="tanggal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal:</label>
+                    <input type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="tanggal" name="tanggal" required placeholder="Pilih Tanggal">
                 </div>
 
                 <!-- Waktu -->
-                <div class="mb-4">
-                    <label for="waktu" class="block text-sm font-medium text-gray-600">Waktu:</label>
-                    <input type="time" class="mt-1 p-2 w-full border rounded-md" id="waktu" name="waktu" required placeholder="Pilih Waktu">
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                    <label for="waktu" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Waktu:</label>
+                    <input type="time" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="waktu" name="waktu" required placeholder="Pilih Waktu">
                 </div>
 
                 <!-- Subject -->
-                <div class="mb-4">
-                    <label for="title" class="block text-sm font-medium text-gray-600">Subject:</label>
-                    <input type="text" class="mt-1 p-2 w-full border rounded-md" id="title" name="title" required placeholder="Masukkan Subject">
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                    <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Subject:</label>
+                    <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="title" name="title" required placeholder="Masukkan Subject">
                 </div>
 
                 <!-- Tempat -->
-                <div class="mb-4">
-                    <label for="tempat" class="block text-sm font-medium text-gray-600">Tempat:</label>
-                    <input type="text" class="mt-1 p-2 w-full border rounded-md" id="tempat" name="tempat" required placeholder="Masukkan Tempat">
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                    <label for="tempat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tempat:</label>
+                    <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="tempat" name="tempat" required placeholder="Masukkan Tempat">
                 </div>
 
                 <!-- Deskripsi -->
-                <div class="mb-4">
-                    <label for="deskripsi" class="block text-sm font-medium text-gray-600">Deskripsi:</label>
-                    <textarea class="mt-1 p-2 w-full border rounded-md" id="deskripsi" name="deskripsi" rows="4" required placeholder="Masukkan Deskripsi"></textarea>
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                    <label for="deskripsi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi:</label>
+                    <textarea class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="deskripsi" name="deskripsi" rows="4" required placeholder="Masukkan Deskripsi"></textarea>
                 </div>
 
                 <!-- Tombol Aksi -->
